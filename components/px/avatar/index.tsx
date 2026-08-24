4347d828649508bf9af0cc9eeab5add820512449
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-24 02:58:50 +0000

    Enlarge homepage profile photo

--- components/px/avatar/index.tsx
+++ components/px/avatar/index.tsx
@@ -2,7 +2,7 @@ import { AvatarDecodeCanvas } from "./avatar-canvas";
 
 export function PixelAvatar() {
   return (
-    <div className="relative flex h-14 w-14 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-zinc-100 dark:bg-zinc-900">
+    <div className="relative flex h-21 w-21 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-zinc-100 dark:bg-zinc-900">
       <span className="sr-only">Portrait of Mark Stuart</span>
       <div aria-hidden="true">
         <AvatarDecodeCanvas src="/avatar.png" />

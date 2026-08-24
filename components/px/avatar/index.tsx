16539ea628014b0577faad7c389165cd5a04d883
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-24 02:24:50 +0000

    Replace profile photo with pixel art

--- components/px/avatar/index.tsx
+++ components/px/avatar/index.tsx
@@ -5,7 +5,7 @@ export function PixelAvatar() {
     <div className="relative flex h-14 w-14 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-zinc-100 dark:bg-zinc-900">
       <span className="sr-only">Portrait of Mark Stuart</span>
       <div aria-hidden="true">
-        <AvatarDecodeCanvas src="/avatar.jpg" />
+        <AvatarDecodeCanvas src="/avatar.png" />
       </div>
     </div>
   );
